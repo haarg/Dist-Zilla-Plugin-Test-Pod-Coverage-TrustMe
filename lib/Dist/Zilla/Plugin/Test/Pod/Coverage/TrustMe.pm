1fbaef4209e3619e0e11a7690c081319fb798575
Graham Knop <haarg@haarg.org>
2024-10-09 06:34:39 +0200

    increment $VERSION after v1.0.0 release

--- lib/Dist/Zilla/Plugin/Test/Pod/Coverage/TrustMe.pm
+++ lib/Dist/Zilla/Plugin/Test/Pod/Coverage/TrustMe.pm
@@ -3,7 +3,7 @@ use warnings;
 use experimental qw(signatures postderef);
 package Dist::Zilla::Plugin::Test::Pod::Coverage::TrustMe;
 
-our $VERSION = 'v1.0.0';
+our $VERSION = 'v1.0.1';
 
 {
     use Moose;
